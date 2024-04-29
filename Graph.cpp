3e256faff52addb048142f5770e13a307ce2e7a2
DannyIsa <daniisakov@gmail.com>
2024-04-29 19:56:02 +0300

    initial

---
+++ Graph.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+
+class Graph {
+    private:
+        int num_vertices;
+        std::vector<std::vector<int>> graph;
+
+    public:
+        Graph(int num_vertices) : num_vertices(num_vertices) {
+            graph.resize(num_vertices);
+        }
+
+        void loadGraph(const std::vector<std::vector<int>>& matrix) {
+            for (int i = 0; i < num_vertices; ++i) {
+                for (int j = 0; j < num_vertices; ++j) {
+                    if (matrix[i][j] != 0 != 0) {
+                        graph[i].push_back(j);
+                    }
+                }
+            }
+        }
+
+        void printGraph() {
+            for (int i = 0; i < num_vertices; ++i) {
+                std::cout << "Neighbors of vertex " << i << ": ";
+                for (int j : graph[i]) {
+                    std::cout << j << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+};
